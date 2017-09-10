9aeb0b1a7a500a30a211c25f265fcf818008fccc
Farhana <farhana@giosg.com>
2017-09-10 16:21:52 +0300

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import unittest
+import time
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,21 +14,45 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
-        # Edith has heard about a cool new online to-do app. She goes
+        # Wafa has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://127.0.0.1:8000')
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She  is invited to enter to do item straight away
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to do item'
 
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
+            )
 
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
+        # She types "Clean the house" into a text box
+        inputbox.send_keys('Clean the house')
+
+        # When she hits enter, the page updates, and now the page lists this as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        self.fail('Finish the test!')
+
+
+
+
+
+
+
+
 
 # There is still a text box inviting her to add another item. She
 # enters "Use peacock feathers to make a fly" (Edith is very methodical)
