4f08795a2c6d351281767ac042932546884a4e7e
Farhana <farhana@giosg.com>
2017-09-17 17:09:44 +0300

    Changed the voodo sleep in functional tests, added fuctionality to handle exceptions due to timeout

--- functional_tests.py
+++ functional_tests.py
@@ -3,13 +3,9 @@ import unittest
 import time
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
-from selenium.common.exceptions import WebDriverException
-
 
 
 class NewVisitorTest(unittest.TestCase):
-
-
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -17,24 +13,16 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def wait_for_row_in_list_table(self, row_text):
-        MAX_WAIT = 10
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except(AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Wafa has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get('http://localhost:8000')
+
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -53,6 +41,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and now the page lists
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(10)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
