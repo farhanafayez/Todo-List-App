0081d9e33f08973eb057a122f4560b893ab44308
Farhana <farhana@giosg.com>
2017-10-08 14:33:33 +0300

    format changes

--- lists/views.py
+++ lists/views.py
@@ -9,17 +9,19 @@ def home_page(request):
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    x  = list_.id
-    return redirect('/lists/'+x+'/')
+    x = list_.id
+    return redirect('/lists/{}/'.format(x))
 
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    x  = list_.id
-    return redirect('/lists/'+x+'/')
+    x = list_.id
+    return redirect('/lists/{}/'.format(x))
 
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     return render(request, 'list.html', {'list': list_})
+
+
